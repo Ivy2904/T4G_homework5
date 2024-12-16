fd994cd90c8a2db3088f3d2f9b063753d7f2a55f
Ivy2904 <ivymarfo23@gmail.com>
2024-12-16 13:49:06 +0000

    complete

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let name = prompt("What is your name?");
+let age = prompt("How old are you");
+let email = prompt ("What is your email");
+// if (name && age && email)
+
+ alert (`Hi your name is ${friend},you are ${age} years old and we will keep in touch via your email: ${email}`) 
+
+ 
